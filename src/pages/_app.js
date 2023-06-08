73ce484197c86cdf6dce261aaf0fbbf4189c5ab3
agahovics <agahkrgz@gmail.com>
2023-06-08 23:39:26 +0300

    add initial files for toDoListApp

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,9 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+import "primereact/resources/primereact.min.css";                                       
+import "/node_modules/primeflex/primeflex.css";
+import "primeflex/themes/primeone-light.css";    
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
